03ead7c183c8ff68cc150f8adea3d8c6ee8a54c6
kakkerlakgly <kristian@rastrup.net>
2015-07-05 21:30:10 +0200

    Added new streams to ResourceTracker so that they are disposed once no longer needed Changed stream in ContentWriter from MemoryStream to VirtualStream

--- Src/ManageMessageNamespace/ContentWriter.cs
+++ Src/ManageMessageNamespace/ContentWriter.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.BizTalk.Streaming;
+using System;
 using System.IO;
 using System.Text;
 using System.Xml;
 
 namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
 {
-    class ContentWriter
+    internal class ContentWriter
     {
         private static NavigationHistoryManager _navigationHistoryManager;
 
@@ -19,9 +20,11 @@ namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
             return MessageWriter(WriteModifyNamespaceElements, stream, encoding, namespaceToModify, newNamespace);
         }
 
-        public Stream AddNamespace(Stream stream, string newNamespace, NamespaceFormEnum namespaceForm, string xPath, Encoding encoding)
+        public Stream AddNamespace(Stream stream, string newNamespace, NamespaceFormEnum namespaceForm, string xPath,
+            Encoding encoding)
         {
-            return MessageWriter(WriteAddNamespaceElements, stream, encoding, newNamespace: newNamespace, namespaceForm: namespaceForm, xPath: xPath);
+            return MessageWriter(WriteAddNamespaceElements, stream, encoding, newNamespace: newNamespace,
+                namespaceForm: namespaceForm, xPath: xPath);
         }
 
         public Stream RemoveNamespace(Stream stream, Encoding encoding)
@@ -29,13 +32,16 @@ namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
             return MessageWriter(WriteRemoveNamespaceElements, stream, encoding);
         }
 
-        private static Stream MessageWriter(Action<XmlWriter, XmlReader, string, string, NamespaceFormEnum, string> writeElement, Stream stream, Encoding encoding, string nsToModify = null, string newNamespace = null, NamespaceFormEnum namespaceForm = NamespaceFormEnum.Unqualified, string xPath = "")
+        private static Stream MessageWriter(
+            Action<XmlWriter, XmlReader, string, string, NamespaceFormEnum, string> writeElement, Stream stream,
+            Encoding encoding, string nsToModify = null, string newNamespace = null,
+            NamespaceFormEnum namespaceForm = NamespaceFormEnum.Unqualified, string xPath = "")
         {
-            var outStream = new MemoryStream();
+            var outStream = new VirtualStream();
 
             using (var reader = XmlReader.Create(stream))
             {
-                using (var writer = XmlWriter.Create(outStream, new XmlWriterSettings { Encoding = encoding }))
+                using (var writer = XmlWriter.Create(outStream, new XmlWriterSettings {Encoding = encoding}))
                 {
                     while (reader.Read())
                     {
@@ -44,12 +50,16 @@ namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
                             case XmlNodeType.CDATA:
                                 writer.WriteCData(reader.Value);
                                 break;
+
                             case XmlNodeType.Comment:
                                 writer.WriteComment(reader.Value);
                                 break;
+
                             case XmlNodeType.DocumentType:
-                                writer.WriteDocType(reader.Name, reader.GetAttribute("PUBLIC"), reader.GetAttribute("SYSTEM"), reader.Value);
+                                writer.WriteDocType(reader.Name, reader.GetAttribute("PUBLIC"),
+                                    reader.GetAttribute("SYSTEM"), reader.Value);
                                 break;
+
                             case XmlNodeType.Element:
                                 var isEmpty = reader.IsEmptyElement;
 
@@ -59,27 +69,34 @@ namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
                                 while (reader.MoveToNextAttribute())
                                 {
                                     // Copy all attributed that aren't namespaces
-                                    if (reader.Value != nsToModify && reader.NamespaceURI != "http://www.w3.org/2000/xmlns/")
-                                        writer.WriteAttributeString(reader.Prefix, reader.LocalName, reader.NamespaceURI, reader.Value);
+                                    if (reader.Value != nsToModify &&
+                                        reader.NamespaceURI != "http://www.w3.org/2000/xmlns/")
+                                        writer.WriteAttributeString(reader.Prefix, reader.LocalName, reader.NamespaceURI,
+                                            reader.Value);
                                 }
 
                                 if (isEmpty)
                                     writer.WriteEndElement();
 
                                 break;
+
                             case XmlNodeType.EndElement:
                                 writer.WriteFullEndElement();
                                 break;
+
                             case XmlNodeType.EntityReference:
                                 writer.WriteEntityRef(reader.Name);
                                 break;
+
                             case XmlNodeType.XmlDeclaration:
                             case XmlNodeType.ProcessingInstruction:
                                 writer.WriteProcessingInstruction(reader.Name, reader.Value);
                                 break;
+
                             case XmlNodeType.Text:
                                 writer.WriteString(reader.Value);
                                 break;
+
                             case XmlNodeType.SignificantWhitespace:
                             case XmlNodeType.Whitespace:
                                 writer.WriteWhitespace(reader.Value);
@@ -95,34 +112,46 @@ namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
             return outStream;
         }
 
-        private void WriteRemoveNamespaceElements(XmlWriter writer, XmlReader reader, string namespaceToModify, string namespaceToAdd, NamespaceFormEnum namespaceForm, string xPath)
+        private void WriteRemoveNamespaceElements(XmlWriter writer, XmlReader reader, string namespaceToModify,
+            string namespaceToAdd, NamespaceFormEnum namespaceForm, string xPath)
         {
             writer.WriteStartElement(reader.LocalName);
         }
 
         // TODO: possible imporvment is to have parameter for naming of namespace
         // Possible issue: What happens if there already is a ns0, is it ok to redefine?
-        private void WriteAddNamespaceElements(XmlWriter writer, XmlReader reader, string namespaceToModify, string namespaceToAdd, NamespaceFormEnum namespaceForm, string xPath)
+        private void WriteAddNamespaceElements(XmlWriter writer, XmlReader reader, string namespaceToModify,
+            string namespaceToAdd, NamespaceFormEnum namespaceForm, string xPath)
         {
             const string prefix = "ns0";
-            var path = _navigationHistoryManager.Add(new NavgiationHistoryItem(reader.Depth, reader.LocalName, reader.NamespaceURI));
+            var path =
+                _navigationHistoryManager.Add(new NavgiationHistoryItem(reader.Depth, reader.LocalName,
+                    reader.NamespaceURI));
 
             if (string.IsNullOrEmpty(xPath) && reader.NamespaceURI != string.Empty && reader.Depth == 0)
-                throw new InvalidOperationException(string.Format("Can not add an new root namespace as a namespace already exists ({0})", reader.NamespaceURI));
+                throw new InvalidOperationException(
+                    string.Format("Can not add an new root namespace as a namespace already exists ({0})",
+                        reader.NamespaceURI));
 
             if ((!string.IsNullOrEmpty(xPath)) && path.IsMatch(xPath) && reader.NamespaceURI != string.Empty)
-                throw new InvalidOperationException(string.Format("Can not add an new namespace to node '{0}' as a namespace already exists ({1})", reader.NamespaceURI, reader.Name));
+                throw new InvalidOperationException(
+                    string.Format("Can not add an new namespace to node '{0}' as a namespace already exists ({1})",
+                        reader.NamespaceURI, reader.Name));
 
             // Add namespace depending on the form of namespace
-            if ((!string.IsNullOrEmpty(xPath) && path.IsMatch(xPath) || (string.IsNullOrEmpty(xPath) && reader.Depth == 0)) && namespaceForm == NamespaceFormEnum.Default)
+            if ((!string.IsNullOrEmpty(xPath) && path.IsMatch(xPath) ||
+                 (string.IsNullOrEmpty(xPath) && reader.Depth == 0)) && namespaceForm == NamespaceFormEnum.Default)
             {
                 writer.WriteStartElement(reader.LocalName, namespaceToAdd);
             }
-            else if ((!string.IsNullOrEmpty(xPath) && path.IsMatch(xPath) || (string.IsNullOrEmpty(xPath) && reader.Depth == 0)) && namespaceForm == NamespaceFormEnum.Unqualified)
+            else if ((!string.IsNullOrEmpty(xPath) && path.IsMatch(xPath) ||
+                      (string.IsNullOrEmpty(xPath) && reader.Depth == 0)) &&
+                     namespaceForm == NamespaceFormEnum.Unqualified)
             {
                 writer.WriteStartElement(prefix, reader.LocalName, namespaceToAdd);
             }
-            else if ((!string.IsNullOrEmpty(xPath) && (path.IsMatch(xPath) || path.IsChildToMatching(xPath)) || string.IsNullOrEmpty(xPath)) && namespaceForm == NamespaceFormEnum.Qualified)
+            else if ((!string.IsNullOrEmpty(xPath) && (path.IsMatch(xPath) || path.IsChildToMatching(xPath)) ||
+                      string.IsNullOrEmpty(xPath)) && namespaceForm == NamespaceFormEnum.Qualified)
             {
                 writer.WriteStartElement(prefix, reader.LocalName, namespaceToAdd);
             }
@@ -142,7 +171,8 @@ namespace BizTalkComponents.PipelineComponents.ManageMessageNamespace
             }
         }
 
-        private void WriteModifyNamespaceElements(XmlWriter writer, XmlReader reader, string namespaceToModify, string namespaceToAdd, NamespaceFormEnum namespaceForm, string xPath)
+        private void WriteModifyNamespaceElements(XmlWriter writer, XmlReader reader, string namespaceToModify,
+            string namespaceToAdd, NamespaceFormEnum namespaceForm, string xPath)
         {
             var ns = "";
             if (reader.NamespaceURI == namespaceToModify)
